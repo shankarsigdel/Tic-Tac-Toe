a70f11454773ca606233ff488a59856925eed7db
Shankar <shankarsigdel26@gmail.com>
2021-07-02 17:30:45 -0400

    Some updates

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-const X_CLASS = 'x'
-const CIRCLE_CLASS = 'circle'
-const WINNING_COMBINATIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle';
+const WINNING_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
-const winningMessageElement = document.getElementById('winningMessage')
-const restartButton = document.getElementById('restartButton')
-const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
-let circleTurn
+const winningMessageElement = document.getElementById('winningMessage');
+const restartButton = document.getElementById('restartButton');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+let circleTurn;
 
-startGame()
+startGame();
 
-restartButton.addEventListener('click', startGame)
+restartButton.addEventListener('click', startGame);
 
 function startGame() {
     circleTurn = false
@@ -29,8 +20,8 @@ function startGame() {
         cell.removeEventListener('click', handleClick)
         cell.addEventListener('click', handleClick, { once: true})
    })
-    setBoardHoverClass()
-    winningMessageElement.classList.remove('show')
+    setBoardHoverClass();
+    winningMessageElement.classList.remove('show');
 }
 
 function handleClick(e) {
